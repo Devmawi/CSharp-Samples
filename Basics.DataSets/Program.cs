d139aabc542105c66602a6cea2024219a0d6c112
Dev <dev@dev.dev>
2021-12-05 22:35:01 +0100

    add basic ado.net sample

---
+++ Basics.DataSets/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Basics.DataSets;
+using Basics.DataSets.DemoDataSetTableAdapters;
+using static System.Console;
+
+Console.WriteLine("Fill DataSet!");
+
+var ds = new DemoDataSet();
+var sampleAdapter = new SampleTableAdapter();
+sampleAdapter.Insert(2, DateTime.Now);
+sampleAdapter.Fill(ds.Sample);
+
+foreach (var item in ds.Sample)
+{
+    WriteLine($"{item.Value}, {item.DateTime}");
+}
